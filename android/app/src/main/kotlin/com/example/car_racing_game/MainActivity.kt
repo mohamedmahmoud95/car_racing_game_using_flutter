7cee0485935ad891e55e43cb3bd4d7797ff6e9a1
mohamreek <46673407+mohamedmahmoud95@users.noreply.github.com>
2023-04-28 15:55:40 +0300

    initial project commit

---
+++ android/app/src/main/kotlin/com/example/car_racing_game/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.car_racing_game
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/car.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Car extends StatelessWidget {
+  const Car({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+
+      ),
+    );
+  }
+}
+
